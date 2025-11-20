32b1f4f4a158339d52cd35fac312b3ebfa3204c2
Kumarrayal123 <kardamkumar13@gmail.com>
2025-11-21 01:14:35 +0530

    kk

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Link}  from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import HeaderTopbar from '../HeaderTopbar'
 import MobileMenu from '../../components/MobileMenu'
 import min1 from '../../images/shop/mini-cart/img-1.jpg'
@@ -7,75 +7,88 @@ import min2 from '../../images/shop/mini-cart/img-2.jpg'
 import './style.css'
 
 const Header = () => {
-    const SubmitHandler = (e) =>{
+    const SubmitHandler = (e) => {
         e.preventDefault()
-     }
+    }
 
-    return(	
-	<div className="middle-header header-style-3">
-        <HeaderTopbar/>
-        <div className="container">
-            <div className="header-content">
-            <div className="row">
-                
-                <div className="col-lg-12 d-lg-block d-none">
-                
+    return (
+        <div className="middle-header header-style-3">
+            <HeaderTopbar />
+            <div className="container">
+                <div className="header-content">
+                    <div className="row">
 
-                      
-                    <nav className='row'>
-                        <ul className="col-lg-9">
-                            <li><Link className="active" to="/about" title="">About Us</Link>
-                              
-                            </li>
-                            <li><Link to="/case" className='drop-downn' title="">Our Programs</Link>
-                                <ul>
-                                    <li><Link to="/case" title="">Causes</Link></li>
-                                    <li><Link to="/case-single" title="">Causes Single</Link></li>
+                        <div className="col-lg-12 d-lg-block d-none">
+
+
+
+                            <nav className='row'>
+                                <ul className="col-lg-9">
+                                    <li><Link className="active" to="/about" title="">About Us</Link>
+
+                                    </li>
+                                    <li><Link to="/case" className='drop-downn' title="">Our Programs</Link>
+                                        <ul>
+                                            <li><Link to="/case" title="">Supporting Disability</Link></li>
+                                            <li><Link to="/case-single" title="">Education</Link></li>
+                                            <li><Link to="/case-single" title="">Medical Support</Link></li>
+                                            <li><Link to="/case-single" title="">Medical Support</Link></li>
+                                            <li><Link to="/case-single" title="">Child trafficking</Link></li>
+                                            <li><Link to="/case-single" title="">Trip to India</Link></li>
+                                            <li><Link to="/case-single" title="">Before/ After school progr</Link></li>
+                                            <li><Link to="/case-single" title="">Disaster emergency relie</Link></li>
+                                        </ul>
+                                    </li>
+                                    <li><Link to="/case" className='drop-downn' title="">Our Partners</Link>
+                                        <ul>
+                                            <li><Link to="/case" title="">Center GC</Link></li>
+                                            <li><Link to="/case-single" title="">Pavani</Link></li>
+                                            <li><Link to="/case-single" title="">CARDS</Link></li>
+                                            <li><Link to="/case-single" title="">Asha jyothi</Link></li>
+                                            <li><Link to="/case-single" title="">TrueBlue</Link></li>
+                                            <li><Link to="/case-single" title="">Emmi Group</Link></li>
+
+                                        </ul>
+                                    </li>
+                                    <li><Link to="/event" title="">Our Impacts</Link>
+
+                                    </li>
+                                    <li><Link to="/home" title="">Stay Informed</Link>
+
+                                    </li>
+
+                                    <li><Link to="/contact" title="">How to be involved?</Link></li>
                                 </ul>
-                            </li>
-                            <li><Link to="/case" className='drop-downn' title="">Our Partners</Link>
-                                <ul>
-                                    <li><Link to="/case" title="">Causes</Link></li>
-                                    <li><Link to="/case-single" title="">Causes Single</Link></li>
+                                <ul className="col-lg-3">
+                                    <li><Link to="/case" className='drop-downn' title="">Resources</Link>
+                                        <ul>
+                                            <li><Link to="/case" title="">Gallery</Link></li>
+                                            <li><Link to="/case-single" title="">Video</Link></li>
+                                            <li><Link to="/case-single" title="">Brochures</Link></li>
+                                            <li><Link to="/case-single" title="">Flyers</Link></li>
+                                        </ul>
+                                    </li>
+                                    <li><Link to="/contact" title="">F.A.Q</Link></li>
+
+                                    <li><Link to="/contact" title="">Subscribe</Link></li>
+
                                 </ul>
-                            </li>
-                            <li><Link to="/event" title="">Our Impacts</Link>
-                              
-                            </li>
-                            <li><Link to="/home" title="">Stay Informed</Link>
-                                
-                            </li>
-                           
-                            <li><Link to="/contact" title="">How to be involved?</Link></li>
-                        </ul>
-                        <ul className="col-lg-3">
-<li><Link to="/case" className='drop-downn' title="">Resources</Link>
-                                <ul>
-                                    <li><Link to="/case" title="">Causes</Link></li>
-                                    <li><Link to="/case-single" title="">Causes Single</Link></li>
-                                </ul>
-                            </li> 
-                                                        <li><Link to="/contact" title="">F.A.Q</Link></li>
+                            </nav>
 
-                            <li><Link to="/contact" title="">Subscribe</Link></li>
 
-                            </ul>
-                    </nav>
-                
-                     
 
-</div>
- <div className="col-md-2 col-sm-2 col-2">
-                    <MobileMenu/>
+                        </div>
+                        <div className="col-md-2 col-sm-2 col-2">
+                            <MobileMenu />
+                        </div>
+                    </div>
+
+
                 </div>
-                </div>
-               
-                
-            </div>
-               
+
                 <div className="clearfix"></div>
             </div>
-            
+
         </div>
     )
 }
