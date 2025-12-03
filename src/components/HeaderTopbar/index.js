3ef2e29262348ee1e9e80b9fbacc7079e8746834
Patternscompany <pandureddypatterns@gmail.com>
2025-12-03 21:05:31 +0530

    pp

--- src/components/HeaderTopbar/index.js
+++ src/components/HeaderTopbar/index.js
@@ -18,7 +18,7 @@ const HeaderTopbar = () => {
                     <div className="col col-md-6 col-sm-5 col-5">
                         <div className="contact-info">
                             <ul className="contact-list d-flex align-items-center justify-content-end">
-                                <li className='d-none d-lg-block' style={{background:' #F8F0FF',color:'#832AD1',padding:'12px 20px',border:"1px solid #832AD1",borderRadius:"8px"}}> <Link to="/Contactus">Contact</Link></li>
+                                <li className='d-none d-lg-block' style={{background:' #F8F0FF',color:'#832AD1',padding:'12px 20px',border:"1px solid #832AD1",borderRadius:"8px"}}> <Link to="/Contactus" style={{color:"#832AD1"}}>Contact</Link></li>
                                 <li style={{background:' #91278f;'}}><Link className="theme-btn m-0" to="/donate">Join Hands</Link></li>
                             
                             </ul>
